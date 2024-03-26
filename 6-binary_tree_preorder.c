846c1e39d936dfe19efd06be97379b08a4684a1d
Gerges-Creative <gerges.zechariah@gmail.com>
2024-03-26 14:48:58 -0700

    Pre-order traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -16,7 +16,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	func(tree);
+	func(tree->n);
 
 	if (tree->left != NULL)
 		binary_tree_preorder(tree->left, &func);
