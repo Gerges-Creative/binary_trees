58601179a0f8dc6addcc26cad4b2bda99789e26d
ziad <ziadabdelmabood@gmail.com>
2024-03-27 03:40:57 +0200

    proj 0x1D Binary trees, task 11 fix style

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -22,7 +22,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 		size_right = binary_tree_size(tree->right);
 	if (tree->left)
 		size_left = binary_tree_size(tree->left);
-	
+
 	/* sum of right and left trees and the root node */
 	size += size_right + size_left;
 	return (size);
